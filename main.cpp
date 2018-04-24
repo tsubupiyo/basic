c18904cace4bfaa36f002d79b669593970ecc108
mituhashi <s157063@toyaku.ac.jp>
2018-04-24 13:19:25 +0900

    周期条件(呼び出し/途中)

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@
 #include <fstream>
 #include "Vector3D.hpp"
 
-constexpr double DELTA         = 10.0;       /*範囲*/
-constexpr int    NUM_STEP      = 100;        /*ステップ数*/
-constexpr int    OUT_FREQ      = 10;		/*出力頻度*/
-constexpr int    SEED          = 140838812;     /*シード*/
-constexpr int    NUM_PARTICLES = 10;   /*粒子数*/
-constexpr double CELL_LENGTH   = 10;   /*セルの長さ*/	 
+constexpr double DELTA         = 10.0;			/*範囲*/
+constexpr int    NUM_STEP      = 100;			/*ステップ数*/
+constexpr int    OUT_FREQ      = 10;			/*出力頻度*/
+constexpr int    SEED          = 140838812;		/*シード*/
+constexpr int    NUM_PARTICLES = 10;			/*粒子数*/
+constexpr double CELL_LENGTH   = 10;			/*セルの長さ*/	 
 std::mt19937 mt(SEED);
 
 void output_structure(const std::string& file_name, const int& step, const std::vector<Vector3D>& ps);
@@ -56,6 +56,7 @@ int main()
    return 0;
 }
 
+//出力
 void output_structure(const std::string& file_name, const int& step, const std::vector<Vector3D>& ps)
 {
    if(0==step)
@@ -74,13 +75,38 @@ void output_structure(const std::string& file_name, const int& step, const std::
    return ;   
 }
 
+//周期境界条件(移動)
 void pbc(Vector3D& point)
 {
+	//各座標が CELL_LENGTH より大きい場合
 	if(point.x > CELL_LENGTH){point.x -= CELL_LENGTH;}
 	if(point.y > CELL_LENGTH){point.y -= CELL_LENGTH;}
 	if(point.z > CELL_LENGTH){point.z -= CELL_LENGTH;}
 	
-	if(point.x <= CELL_LENGTH){point.x += CELL_LENGTH;}
-	if(point.y <= CELL_LENGTH){point.y += CELL_LENGTH;}
-	if(point.z <= CELL_LENGTH){point.z += CELL_LENGTH;}
+	//各座標が 0 以下の場合
+	if(point.x <= 0){point.x += CELL_LENGTH;}	
+	if(point.y <= 0){point.y += CELL_LENGTH;}
+	if(point.z <= 0){point.z += CELL_LENGTH;}	
+}
+
+//周期境界条件(呼び出し)
+void pbc_call(const Vector3D& point)
+{	
+	Vector3D image;			/*イメージ粒子*/
+	int i = 0;
+
+	for(int x = -1; x <= 1; ++x)
+	{
+		for(int y = -1; y <= 1; ++y)
+		{
+			for(int z = -1; z <= 1; ++z)
+			{
+				image.at(i).x = point.x + CELL_LENGTH * x;
+				image.at(i).y = point.y + CELL_LENGTH * y;
+				image.at(i).z = point.z + CELL_LENGTH * z;
+				i++;
+			}
+		}
+	}
+//この後??
 }
