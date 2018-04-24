7489487474c796b40cf54a83c8bf61e9802a7e48
mituhashi <mituhashi@dhcp2024075.ls.toyaku.ac.jp>
2018-04-24 10:57:53 +0900

    周期境界条件の追加(移動)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//粒子N個の座標を動かす
+//粒子N個の座標を動かす(周期境界条件)
 #include <iostream>
 #include <cmath>
 #include <vector>
@@ -8,26 +8,28 @@
 
 constexpr double DELTA         = 10.0;       /*範囲*/
 constexpr int    NUM_STEP      = 100;        /*ステップ数*/
-constexpr int    OUT_FREQ      = 10;
+constexpr int    OUT_FREQ      = 10;		/*出力頻度*/
 constexpr int    SEED          = 140838812;     /*シード*/
 constexpr int    NUM_PARTICLES = 10;   /*粒子数*/
-
+constexpr double CELL_LENGTH   = 10;   /*セルの長さ*/	 
 std::mt19937 mt(SEED);
 
 void output_structure(const std::string& file_name, const int& step, const std::vector<Vector3D>& ps);
+
+void pbc(Vector3D& point);
 
 int main()
 {
    std::vector<Vector3D> particles(NUM_PARTICLES); /*N個の粒子*/
 
    const std::string file_name = "hoo_INFO.dat";
-
+/*
    const Vector3D wtf(5.0,5.0,5.0);
    for(int i = 1; i < NUM_PARTICLES; ++i)
    {
       particles.at(i)=i*wtf;
    }
-
+*/
    std::uniform_real_distribution<double> dist_d(-0.5*DELTA,+0.5*DELTA);
    Vector3D d;
    output_structure(file_name,0,particles);
@@ -42,6 +44,7 @@ int main()
             d.z = dist_d(mt);
          } while (d.norm() >= 0.5*DELTA);
          particles.at(ptcl)+=d;
+		 pbc(particles.at(ptcl));
       }
       if((step%OUT_FREQ==0) && step!=0)
       {
@@ -69,4 +72,15 @@ void output_structure(const std::string& file_name, const int& step, const std::
    ofs<<std::endl;
    ofs.close();
    return ;   
+}
+
+void pbc(Vector3D& point)
+{
+	if(point.x > CELL_LENGTH){point.x -= CELL_LENGTH;}
+	if(point.y > CELL_LENGTH){point.y -= CELL_LENGTH;}
+	if(point.z > CELL_LENGTH){point.z -= CELL_LENGTH;}
+	
+	if(point.x <= CELL_LENGTH){point.x += CELL_LENGTH;}
+	if(point.y <= CELL_LENGTH){point.y += CELL_LENGTH;}
+	if(point.z <= CELL_LENGTH){point.z += CELL_LENGTH;}
 }
